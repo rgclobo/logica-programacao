aaa9a5a3f4679d6b21e9413eec98aa4679d5ed9c
rgclobo <rick.gimenez@hotmail.com>
2025-05-13 00:09:39 -0300

    Variables and data types

---
+++ 03_variaveis_tipo_dados.py
@@ -0,0 +1,20 @@
+print("Hello world!")
+print("Eu de novo")
+print(1+1)
+x = 15
+print(x * 2)
+
+#TIPOS DE DADOS
+## Numéricos, texto, booleanos (lógico)
+
+#Linguagens estaticamente tipadas:
+## É preciso declarar o tipo de dados antes de usar
+###EX: Java, C, C++, Go, Rust
+
+#Linguagens dinamicamente tipadas:
+## Não é preciso declarar o tipo de dados antes de usar
+###EX: Python, Ruby, JavaScript e PHP
+
+#Existem linguagens fortemente tipadas e fracamente tipadas.
+## JavaScript por exemplo é fracamente tipada, sendo possível misturar tipos de dados.
+## O Python por sua vez, é uma linguaguem fortemente tipada, que já não permite a mistura de dados.
