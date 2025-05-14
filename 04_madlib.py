33e573f666db4415845f3024e009d5aaeb915778
rgclobo <rick.gimenez@hotmail.com>
2025-05-14 14:46:54 -0300

    Create Madlib game in Python

---
+++ 04_madlib.py
@@ -0,0 +1,31 @@
+#Jogo de MADLIB, criado em Python, para aprender os conceitos de concatenação e uso de variaveis recebidas pelo usuário.
+
+print('Bem vindo ao nosso jogo de MADLIB!!')
+profissao = input('Digite uma profissão: ')
+nome = input('Digite um nome: ')
+cidade = input('Digite uma cidade: ')
+criatura = input('Digite uma criatura qualquer: ')
+objeto = input('Digite um Objeto muito valioso: ')
+local = input('Digite um local muito famoso: ')
+cor = input('Digite uma cor: ')
+gadget = input('Digite um objeto eletrônico portátil: ')
+transporte = input('Digite um meio de transporte: ')
+inimigo = input('Digite um plural de inimigo: ')
+arma = input('Digite uma arma estranha: ')
+habilidade = input('Digite uma habilidade especial: ')
+frase = input('Digite uma frase de efeito: ')
+numero = input('Digite um número: ')
+comida = input('Digite sua comida favorita: ')
+adjetivo = input('Digite um adjetivo: ')
+
+print ('Madlib: “A Missão Secreta do Agente Improvisado”')
+
+print ('Era uma vez um(a) ' + profissao + ' chamado(a) ' + nome +', que morava na cidade de ' + cidade+'. Certo dia, ele(a) recebeu uma missão secreta do governo para capturar um(a) perigoso(a) ' + criatura + ' que roubou um(a) ' + objeto +' do(a) ' + local+'.')
+
+print ('Com sua roupa especial de cor ' + cor + ' e seu inseparável ' + gadget +', ele(a) embarcou em um(a) ' + transporte + ' rumo ao destino. No caminho, enfrentou um exército de ' + inimigo + ', todos armados com ' + arma + '.')
+
+print ('Usando sua habilidade secreta de ' + habilidade + ', ele(a) conseguiu derrotá-los e continuou sua jornada. Chegando ao esconderijo, surpreendeu o(a) ' + criatura + ' com um grito de guerra: “' + frase + '!”')
+
+print ('Depois de uma batalha épica, que durou ' + numero + ' horas, ele(a) recuperou o(a) ' + objeto + ' e voltou como herói para ' + cidade +', onde foi recebido com uma festa cheia de ' + comida + ' e fogos de artifício.')
+
+print ('E assim, o(a) ' + profissao + ' provou que até mesmo nas situações mais ' + adjetivo + ', a coragem e um pouco de improviso fazem toda a diferença.')
